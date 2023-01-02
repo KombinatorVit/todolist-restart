51fc6d2f1c4d2e18116864f4057d09901b5f13a7
KombinatorVit <slonik0775@gmail.com>
2023-01-03 00:03:42 +0200

    TasksReducer: addTask

--- src/state/task-reducer.ts
+++ src/state/task-reducer.ts
@@ -1,28 +1,36 @@
 import {TaskStateType} from "../App";
+import {v1} from "uuid";
 
-export type RemoveTaskACType = {
-    type: 'REMOVE-TASK'
-    id: string
-    todolistId: string
-}
 
-type ActionType = RemoveTaskACType
+
+type ActionType =
+    ReturnType<typeof removeTaskAC> |
+    ReturnType<typeof addTaskAC>
 export const tasksReducer = (state: TaskStateType, action: ActionType) => {
     switch (action.type) {
         case 'REMOVE-TASK':
             return {...state, [action.todolistId]: state[action.todolistId].filter(el=> el.id !== action.id)}
-
+        case 'ADD-TASK' as const:
+            return {...state, [action.todolistId]: [{id: v1(), title:action.title, isDone: false}, ...state[action.todolistId]]}
         default:
             throw new Error('I don\'t understand this type')
     }
 }
 
 
-export function removeTaskAC(id: string, todolistId: string): RemoveTaskACType{
+export function removeTaskAC(id: string, todolistId: string){
     return {
-        type: 'REMOVE-TASK',
+        type: 'REMOVE-TASK' as const,
         id,
         todolistId
 }
+}
+
+export function addTaskAC(title:string, todolistId: string){
+    return{
+        type: 'ADD-TASK' as const,
+        title,
+        todolistId
+    }
 }
 
