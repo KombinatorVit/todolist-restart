1aec7532e1df2fd7d1dab28a1242bc84652c3de1
KombinatorVit <slonik0775@gmail.com>
2023-01-09 23:05:53 +0200

    Post

--- src/Todolist/todolists-api.stories.tsx
+++ src/Todolist/todolists-api.stories.tsx
@@ -25,6 +25,9 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'newTodolist'}, settings).then((res) => {
+            setState(res.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
